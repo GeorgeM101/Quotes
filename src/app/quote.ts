893e550eb3d48f4cb9b7ff1fd2832184c38c3582
GeorgeM101 <georgmboya@gmail.com>
2022-01-09 23:29:15 +0300

    added placeholders

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,7 @@
 export class Quote {
+  emit(addQuote: Quote) {
+    throw new Error('Method not implemented.');
+  }
   showDescription: boolean;
   constructor(public id: number,public name: string,public description: string, public completeDate: Date){
     this.showDescription=false;
