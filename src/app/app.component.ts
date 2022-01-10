213d4bd6cdc8dc8c44643f6a00e8752db10fb8e3
GeorgeM101 <georgmboya@gmail.com>
2022-01-10 10:40:10 +0300

    added background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core'
-import { Quote } from './quote'
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
   })
   export class AppComponent {
-//   title(title: any) {
-//     throw new Error('Method not implemented.')
-    
+  title(title: any) {
+    throw new Error('Method not implemented.')
+  }
   }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,8 @@ import { Quote } from '../quote'
 export class QuotesComponent {
   quotes:Quote [] = [
     
-    new Quote(1, 'Street', 'Those who lie belongs to the streets',new Date(2021,3,14)),
-    new Quote(2,'Bro Code','Abro shall a comedian when his bro girl is around',new Date(2022,6,9)),
-    new Quote(3,'life','What comes easy won;t last and what comes easy won;t last',new Date(2022,1,12)),
+    new Quote(1, 'Life', 'Do what is right according to you and what aligns to your dreams',new Date(2022,1,3)),
+    new Quote(2,'Wealth','Cut off things that drain you and focus on what builds you',new Date(2022,1,8)),
 
   ];
   
